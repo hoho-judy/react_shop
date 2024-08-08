588dcf19bb35903d8d9d48571eb4d68f98238925
juyeonk <bpcoo99@gmail.com>
2024-08-08 22:23:53 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import "./App.css";
+import { useState } from 'react';
+import { Container, Nav, Navbar, Row, Col } from "react-bootstrap";
+import Card from './components/Card.js';
+import Detail from './components/Detail.js';
+import bgImg from './img/bg.png'; // 이미지를 변수화해서 쓰고 싶을 때
+import datas from './data.js';
+import { Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
+
+  let [shoes] = useState(datas);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Judy's Shoes</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#features">Products</Nav.Link>
+            <Nav.Link href="#pricing">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+     
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className="main-bg" style={{backgroundImage: `url(${bgImg})`}}></div>  
+            <Container>
+              <Row>
+              {
+                shoes.map((shoe)=>{
+                  return (
+                    <Link to="/detail" element={<Detail></Detail>}>
+                      <Card shoe={shoe} />
+                    </Link>
+                  )
+                })
+              }         
+              </Row>
+            </Container>
+          </>
+        } />
+        <Route path="/detail" element={<div>상세페이지</div>} />
+      </Routes>
+ 
     </div>
   );
 }
